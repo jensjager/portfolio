70eeb359a5dc76e134a26c12c494bb2eb2913d13
Jens Jäger <jens.j.jager@gmail.com>
2025-04-23 03:56:30 +0300

    Vaporwave scene with r2f

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,12 @@
-import { createRoot } from 'react-dom/client'
-import { Canvas } from '@react-three/fiber'
-import './styles.css'
+import { createRoot } from 'react-dom/client';
+import GridScene from './scenes/GridScene.tsx';
 
-function App() {
-  return (
-    <div id="canvas-container">
-      <Canvas>
-      </Canvas>
-    </div>
-  )
+export default function App() {
+	return (
+		<div className="w-dvw h-dvh">
+			<GridScene />
+		</div>
+	);
 }
 
-createRoot(document.getElementById('root')!).render(<App />)
+createRoot(document.getElementById('root')!).render(<App />);
